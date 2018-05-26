f04d2f8aa08db84f271cc788f7708f749297c9c6
fdrury <fdrury@ualberta.ca>
2018-05-26 01:24:53 -0600

    Finished basic PlayerEvaluation

--- code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/PlayerEvaluationActivity.java
+++ code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/PlayerEvaluationActivity.java
@@ -2,14 +2,23 @@ package ca.hockeyconnect.hockeyconnect;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class PlayerEvaluationActivity extends AppCompatActivity {
+
+    SeekBar[] seekBars = new SeekBar[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_evaluation);
+
+        seekBars[0] = (SeekBar)findViewById(R.id.seekBar0);
+        seekBars[1] = (SeekBar)findViewById(R.id.seekBar1);
+        seekBars[2] = (SeekBar)findViewById(R.id.seekBar2);
+        seekBars[3] = (SeekBar)findViewById(R.id.seekBar3);
+        seekBars[4] = (SeekBar)findViewById(R.id.seekBar4);
 
         TextView playerNameTextView = (TextView)findViewById(R.id.textViewPlayerName);
         TextView playerNumberTextView = (TextView)findViewById(R.id.textViewPlayerNumber);
@@ -19,6 +28,13 @@ public class PlayerEvaluationActivity extends AppCompatActivity {
         TextView attribute3TextView = (TextView)findViewById(R.id.textViewAttributeName3);
         TextView attribute4TextView = (TextView)findViewById(R.id.textViewAttributeName4);
 
+        final TextView[] attributeValueViews = {(TextView)findViewById(R.id.textViewAttribute0),
+                (TextView)findViewById(R.id.textViewAttribute1),
+                (TextView)findViewById(R.id.textViewAttribute2),
+                (TextView)findViewById(R.id.textViewAttribute3),
+                (TextView)findViewById(R.id.textViewAttribute4)};
+        //final TextView attribute0ValueView = (TextView)findViewById(R.id.textViewAttribute0);
+
         playerNameTextView.setText(R.string.test_player_name_name);
         playerNumberTextView.setText(R.string.test_player_number_name);
         attribute0TextView.setText(R.string.attribute0_name);
@@ -26,5 +42,30 @@ public class PlayerEvaluationActivity extends AppCompatActivity {
         attribute2TextView.setText(R.string.attribute2_name);
         attribute3TextView.setText(R.string.attribute3_name);
         attribute4TextView.setText(R.string.attribute4_name);
+
+        for(int i = 0; i < 5; i++) {
+            final TextView attributeValueView = attributeValueViews[i];
+            seekBars[i].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (progress == 0)
+                        attributeValueView.setText(R.string.not_rated_name);
+                    else
+                        attributeValueView.setText(Integer.toString(progress));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        }
     }
+
+
 }
