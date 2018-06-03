5698e480eb85e504e7256c0f6347625da2db21ef
fdrury <fdrury@ualberta.ca>
2018-06-02 19:21:00 -0600

    Made timer page mostly functional

--- code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/TimerActivity.java
+++ code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/TimerActivity.java
@@ -1,26 +1,47 @@
 package ca.hockeyconnect.hockeyconnect;
 
+import android.content.res.ColorStateList;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TimerActivity extends AppCompatActivity {
 
-    private Chronometer chronometer;
     private boolean timerRunning = false;
+    private long millisecondTime, startTime, timeBuff, updateTime = 0L;
+    int seconds, minutes, milliSeconds;
+
+    Handler handler;
+
+    TextView textView;
+
+    ImageButton helpButton;
+    Button saveButton;
+    Button startStopButton;
+    Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        ImageButton helpButton = (ImageButton)findViewById(R.id.helpButtonTimer);
+        handler = new Handler();
+
+        helpButton = (ImageButton)findViewById(R.id.helpButtonTimer);
+        cancelButton = (Button)findViewById(R.id.buttonCancel);
+        saveButton = (Button)findViewById(R.id.buttonSave);
+        startStopButton = (Button)findViewById(R.id.startStopButton);
+
+        textView = (TextView)findViewById(R.id.textViewTimer);
+
+        saveButton.setEnabled(false);
 
         helpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,34 +50,78 @@ public class TimerActivity extends AppCompatActivity {
             }
         });
 
-        /*chronometer = (Chronometer)findViewById(R.id.chronometer);
-
-        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChronometerTick(Chronometer chronometerChanged) {
-                chronometer = chronometerChanged;
+            public void onClick(View v) {
+                finish();
             }
         });
 
-        final Button startStopButton = (Button)findViewById(R.id.startStopButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: send data back
+                finish();
+            }
+        });
 
         startStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(timerRunning){
-                    chronometer.stop();
+                    // TODO: stop timer
+                    timeBuff += millisecondTime;
+                    handler.removeCallbacks(runnable);
                     timerRunning = false;
+                    saveButton.setEnabled(true);
                     startStopButton.setText("RE-START");
+                    // TODO: use tint instead
                     startStopButton.setBackgroundColor(startStopButton.getContext().getResources().getColor(R.color.colorGreen));
                 }
                 else{
-                    chronometer.setBase(SystemClock.elapsedRealtime());
-                    chronometer.start();
+                    millisecondTime = 0L ;
+                    timeBuff = 0L ;
+                    updateTime = 0L ;
+                    seconds = 0 ;
+                    minutes = 0 ;
+                    milliSeconds = 0 ;
+                    // TODO: start timer
+                    startTime = SystemClock.uptimeMillis();
+                    handler.post(runnable);
+                    saveButton.setEnabled(false);
                     timerRunning = true;
                     startStopButton.setText("STOP");
+                    // TODO: use tint instead
                     startStopButton.setBackgroundColor(startStopButton.getContext().getResources().getColor(R.color.colorRed));
                 }
             }
-        });*/
+        });
+
+
     }
+
+    public Runnable runnable = new Runnable() {
+
+        @Override
+        public void run() {
+            millisecondTime = SystemClock.uptimeMillis() - startTime;
+
+            updateTime = timeBuff + millisecondTime;
+
+            seconds = (int) (updateTime / 1000);
+
+            minutes = seconds / 60;
+
+            seconds = seconds % 60;
+
+            milliSeconds = (int) (updateTime % 1000);
+
+            textView.setText("" + minutes + ":"
+                    + String.format("%02d", seconds) + ":"
+                    + String.format("%03d", milliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
 }
