ce200a769d6cba6aee64e894c3d8442d4dcd1f1f
fdrury <fdrury@ualberta.ca>
2018-08-17 00:47:44 -0600

    Started implementing REST for player list
    Almost fully implemented - still requires testing

--- code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/PlayerListActivity.java
+++ code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/PlayerListActivity.java
@@ -1,16 +1,61 @@
 package ca.hockeyconnect.hockeyconnect;
 
+import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.JsonReader;
+import android.widget.ArrayAdapter;
 
-public class PlayerListActivity extends AppCompatActivity {
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
+public class PlayerListActivity extends ListActivity {
+
+    ArrayList<String> PlayerList = new ArrayList<String>();
+
+    ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_list);
 
-        
+        listAdapter = new ArrayAdapter<String>(this, R.layout.list_item_player, PlayerList);
+        setListAdapter(listAdapter);
+        //ListView listView = (ListView) findViewById(R.id.listView1);
+        //listView.setAdapter(listAdapter);
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL hcServer = new URL("192.168.0.160/tryout/1");
+                    HttpURLConnection serverConnection = (HttpURLConnection) hcServer.openConnection();
+                    serverConnection.setRequestMethod("GET");
+                    InputStream responseBody = serverConnection.getInputStream();
+                    InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                    JsonReader jsonReader = new JsonReader(responseBodyReader);
+
+                    jsonReader.beginObject();
+                    while (jsonReader.hasNext()) {
+                        String key = jsonReader.nextName();
+                        if(key.equals("FirstName")) {
+                            PlayerList.add(jsonReader.nextString());
+                            listAdapter.notifyDataSetChanged();
+                        } else {
+                            jsonReader.skipValue();
+                        }
+                    }
+
+                    jsonReader.close();
+                    serverConnection.disconnect();
+                } catch(Exception e) {
+                    // do nothing
+                }
+            }
+        });
     }
 }
