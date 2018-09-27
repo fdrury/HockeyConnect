e9016e3d32df93f474a71b074b8c157f01c5456c
fdrury <fdrury@ualberta.ca>
2018-09-27 00:17:51 -0600

    Uploading players is now functional
    Need to make more robust. Maybe serve html from Python sript? See flask tutorial for info.

--- code/PythonServer/HockeyConnect.py
+++ code/PythonServer/HockeyConnect.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 import pymssql
 import datetime
+import csv
+import io
 
 app = Flask(__name__)
 
@@ -75,6 +77,40 @@ def saveGameEval():
             cursor.execute('INSERT INTO SkillEvaluations(PlayerID, TryoutID, Speed, HockeyAwareness, CompeteLevel, PuckHandling, Agility, Date) VALUES (%d, %d, %d, %d, %d, %d, %d, %s);', (playerID, tryoutID, speed, hockeyAwareness, competeLevel, puckHandling, agility, currentTime))
             conn.commit()
             return jsonify({'Success' : 1})
+
+@app.route('/uploadPlayers', methods = ['POST'])
+def uploadPlayers():
+    with pymssql.connect(server, user, password, database) as conn:
+        with conn.cursor(as_dict=True) as cursor:
+            if 'csvfile' not in request.files:
+                print('No file found')
+                return 'No file found'
+            f = request.files['csvfile']
+            if not f:
+                return 'No file'
+            #fileContents = f.stream.read().decode('utf-8')
+            fileContents = io.StringIO(f.stream.read().decode('utf-8'), newline=None)
+            
+            cursor.execute('SELECT ID FROM Tryouts ORDER BY ID DESC')
+            row = cursor.fetchone()
+            newTryoutID = row.get('ID', 0) + 1
+            cursor.execute('INSERT INTO Tryouts(ID) VALUES(%d);', newTryoutID)
+
+            cursor.execute('SELECT ID FROM Players ORDER BY ID DESC')
+            row = cursor.fetchone()
+            nextPlayerID = row.get('ID', 0) + 1
+            reader = csv.DictReader(fileContents)
+            for row in reader:
+                rowID = row['ID']
+                if rowID == '':
+                    rowID = str(nextPlayerID)
+                    nextPlayerID += 1
+                cursor.execute('INSERT INTO Players(FirstName, LastName, ID) VALUES(%s, %s, %s);', (row['FirstName'], row['LastName'], rowID))
+                cursor.execute('INSERT INTO PlayerTryouts(PlayerID, TryoutID) VALUES(%s, %d);', (rowID, newTryoutID))
+
+            
+            conn.commit()
+            return ('File uploaded successfully. Your Tryout ID is %d' % newTryoutID)
 
 
 if __name__ == '__main__':
