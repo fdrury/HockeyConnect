9a8495770b7c8688136d8d78ecf9996a08e1a23d
fdrury <fdrury@ualberta.ca>
2018-12-31 19:21:48 -0700

    began implementation of attribute array adapter

--- code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/AttributeAdapter.java
+++ code/HockeyConnect/app/src/main/java/ca/hockeyconnect/hockeyconnect/AttributeAdapter.java
@@ -1,7 +1,11 @@
 package ca.hockeyconnect.hockeyconnect;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -9,4 +13,21 @@ public class AttributeAdapter extends ArrayAdapter<Attribute> {
     public AttributeAdapter(Context context, ArrayList<Attribute> attributes) {
         super(context, 0, attributes);
     }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Attribute attribute = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_attribute_slider, parent, false);
+        }
+        // Lookup view for data population
+        TextView attributeName = (TextView) convertView.findViewById(R.id.textViewAttributeName);
+        // Populate the data into the template view using the data object
+        attributeName.setText(attribute.getAttributeName());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
 }
